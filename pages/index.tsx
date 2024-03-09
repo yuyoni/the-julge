fa586761e1734fa9d216a239d17af999025b1296
jobible <whtjdrud800@gmail.com>
2024-03-10 01:54:17 +0900

    ✨ feat: 실제 페이지에서 확인하기 위해 index 페이지에 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
+import Gnb from "@/components/Gnb";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <>안녕하세여</>;
+  const [user, setUser] = useState<
+    "guest" | "employee" | "employer" | undefined
+  >("guest");
+
+  return (
+    <>
+      <Gnb userType={user} />
+      <button onClick={() => setUser("guest")}>로그인하지 않음</button>
+      <button onClick={() => setUser("employee")}>알바님</button>
+      <button onClick={() => setUser("employer")}>사장님</button>
+    </>
+  );
 }
