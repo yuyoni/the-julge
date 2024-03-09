7016d3c853f4299ed625f841db57f82746f2a547
jobible <whtjdrud800@gmail.com>
2024-03-10 01:37:16 +0900

    💄 design: 공통 텍스트 ts 추가

---
+++ lib/types/text.ts
@@ -0,0 +1,34 @@
+import { css } from '@emotion/react'
+
+export const julgeCaptionStyle = css`
+  font-family: 'Spoqa Han Sans Neo';
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 16px; /* 133.333% */
+`
+
+export const julgeBodyRegular = css`
+  font-family: 'Spoqa Han Sans Neo';
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 22px; /* 157.143% */
+`
+export const julgeBody2Bold = css`
+  font-family: 'Spoqa Han Sans Neo';
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`
+
+/* The julge/h2 */
+export const julgeH2 = css`
+  font-family: 'Spoqa Han Sans Neo';
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: 0.48px;
+`
