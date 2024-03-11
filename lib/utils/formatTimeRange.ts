46a306a086093c723539565ee1a99f1cbdd48448
YU YEON LEE <doli0116@naver.com>
2024-03-11 09:58:29 +0900

    ✨ feat: 날짜 형식 수정
    - .을 이용한 날짜 표시에서 피그마에 나온 하이픈을 이용해 구분한 형식으로 변경

--- lib/utils/formatTimeRange.ts
+++ lib/utils/formatTimeRange.ts
@@ -1,17 +1,17 @@
 export default function formatTimeRange(startsAt: string, workhour: number) {
   const startsTime = new Date(startsAt);
-  const endsTime = new Date(startsAt);
+  const endsTime = new Date(startsTime.getTime() + workhour * 60 * 60 * 1000);
 
-  endsTime.setHours(endsTime.getHours() + workhour);
-
-  const formattedStartsAt = startsTime
-    .toLocaleString("ko-KR", {
-      timeZone: "UTC",
-    })
-    .slice(0, -3);
-  const formattedEndsAt = endsTime
-    .toLocaleString("ko-KR", { timeZone: "UTC" })
-    .slice(0, -3);
+  const formattedStartsAt = formatDate(startsTime);
+  const formattedEndsAt = formatDate(endsTime);
 
   return `(${formattedStartsAt} ~ ${formattedEndsAt})`;
+}
+
+function formatDate(date: Date): string {
+  return date
+    .toLocaleString("ko-KR", { timeZone: "Asia/Seoul" })
+    .replace(". ", "-")
+    .replace(". ", "-")
+    .replace(".", "");
 }
