261c1fbf8af7b8ac2fcfad9d40bc34bdc31f6a2e
괴도소연\박소연 <rnlduq157@naver.com>
2024-03-14 07:22:39 +0900

    ✨ feat: 가게 등록 페이지 구현을 위한 Input 컴포넌트 추가
    - 기존에 제가 만들었던 Input 컴포넌트가 사용하기 어려운 것 같아 테스트
      겸 추가해보았습니다 í´
    - 크기도 지정할 수 있고 제가 고민했던 부분들까지 추가가 가능하긴 한데,
      맞는지는 모르겠네요 더 좋은 방법이 있을 것 같은데 제 한계인 것
      같습니다  í¸­

---
+++ pages/myShop/register/index.tsx
@@ -0,0 +1,17 @@
+import Gnb from "@/components/Gnb";
+import Input from "./components/Input";
+
+export default function Register() {
+  return (
+    <>
+      <Gnb userType="employer" />
+      <h1>가게 정보</h1>
+      <Input label="가게 이름*" />
+      <Input label="분류*" placeholder="선택" includeImage inputSize={600} />
+      <Input label="주소*" placeholder="선택" includeImage />
+      <Input label="기본 시급*" includeText="원" />
+      <Input label="상세 주소*" />
+      <Input label="가게 설명" />
+    </>
+  );
+}
