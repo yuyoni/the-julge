1344675e4a608c99ff9872253369ff7600759ec5
jobible <whtjdrud800@gmail.com>
2024-03-17 01:32:32 +0900

    💄 design: 시급 인상률에 맞춰 Post 카드 Badge 색 변경

--- components/Post/utils/getWageIncreaseText.ts
+++ components/Post/utils/getWageIncreaseText.ts
@@ -2,12 +2,16 @@ const getWageIncreaseText = (hourlyPay: number, originalHourlyPay: number) => {
   const increaseRate =
     ((hourlyPay - originalHourlyPay) / originalHourlyPay) * 100;
 
-  if (increaseRate >= 100) {
-    return "시급 100% ▲";
+  if (increaseRate >= 200) {
+    return 200;
+  } else if (increaseRate >= 100) {
+    return 100;
   } else if (increaseRate >= 50) {
-    return "시급 50% ▲";
+    return 50;
   } else if (increaseRate >= 30) {
-    return "시급 30% ▲";
+    return 30;
+  } else if (increaseRate >= 10) {
+    return 10;
   }
   return null;
 };
