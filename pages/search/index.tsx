5cd704167719a0e70f04572c613353ba951aa48a
jobible <whtjdrud800@gmail.com>
2024-03-17 18:56:27 +0900

    🚧 : 페이지네이션 연동 작업중

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -5,10 +5,11 @@ import { useRouter } from "next/router";
 export default function Search() {
   const router = useRouter();
   const keyword = router.query.keyword as string;
+  const page = parseInt(router.query.page as string, 10);
 
   return (
     <Layout>
-      <AllNotice keyword={keyword} />
+      <AllNotice keyword={keyword} initialPage={page} />
     </Layout>
   );
 }
