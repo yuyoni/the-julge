f93f77f10ccef9fa83c35dec74dd8af9ec9a0ab4
jobible <whtjdrud800@gmail.com>
2024-03-19 00:16:42 +0900

    🚑 hotfix: 무한 로딩 에러 해결 위한 임시 데이터

--- pages/search/components/RecommendNotice/index.tsx
+++ pages/search/components/RecommendNotice/index.tsx
@@ -6,7 +6,7 @@ import PostList from "@/pages/search/components/PostList";
 
 export default function RecommendNotice() {
   // 추후에 로그인을 통해 가져올 데이터(쿠키나 다른 저장소나 상태관리 등)
-  const userId = "52d15203-7313-444e-86ce-881159b851fb";
+  const userId = "0dcb5feb-fe19-4b3d-b318-3a449b023461";
   const { data: userData } = useUserData(userId);
   const { data: noticesData } = useNoticesData(userData?.item?.address);
   const notices = noticesData?.items ?? [];
