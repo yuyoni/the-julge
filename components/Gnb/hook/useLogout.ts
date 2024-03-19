95e8b5ff1978b3aecf8421864acdeb866ed66436
jobible <whtjdrud800@gmail.com>
2024-03-19 23:58:07 +0900

    🐛 bug: 배포 에러 의심 가는 부분 수정

--- components/Gnb/hook/useLogout.ts
+++ components/Gnb/hook/useLogout.ts
@@ -2,10 +2,13 @@ const deleteCookie = (router: {
   push: (arg0: string) => Promise<any>;
   reload: () => any;
 }) => {
-  document.cookie = "id=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-  document.cookie =
-    "accessToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-  document.cookie = "userType=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-  router.push("/").then(() => router.reload());
+  if (typeof window !== "undefined") {
+    document.cookie = "id=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    document.cookie =
+      "accessToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    document.cookie =
+      "userType=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    router.push("/").then(() => router.reload());
+  }
 };
 export default deleteCookie;
