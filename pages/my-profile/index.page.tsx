467b93e8eb83979a21cc21eea3806862c0952497
midcondria <hyukkind@naver.com>
2024-03-21 00:38:08 +0900

    ✨ feat: 페이지네이션 연동

--- pages/my-profile/index.page.tsx
+++ pages/my-profile/index.page.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import ApplyHistory, { ApplyHistoryProps } from "./ApplyHistory";
 import Layout from "@/components/Layout";
 import Profile from "./Profile";
@@ -22,8 +23,14 @@ type Application = {
 };
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const LIMIT = 5;
 
 export default function MyProfile() {
+  const router = useRouter();
+  const { page } = router.query;
+  const currentPage = Number(page) >= 1 ? Number(page) : 1;
+  const offset = 5 * (currentPage - 1);
+
   const { jwt, userType, id } = useCookie();
   const [profile, setProfile] = useState<UserProfile>({
     name: "",
@@ -31,22 +38,27 @@ export default function MyProfile() {
     address: "",
     bio: "",
   });
-  const [applicationHistory, setApplicationHistory] = useState<Application[]>(
-    [],
-  );
+  const [history, setHistory] = useState<ApplyHistoryProps>({
+    type: "employee",
+    limit: LIMIT,
+    count: 0,
+    items: [],
+  });
 
   useEffect(() => {
-    console.log("useEffect");
     const fetchData = async () => {
       try {
         const [{ data: userInfo }, { data: applicationHistory }] =
           await Promise.all([
             axios.get(`${BASE_URL}/users/${id}`),
-            axios.get(`${BASE_URL}/users/${id}/applications`, {
-              headers: {
-                Authorization: jwt,
+            axios.get(
+              `${BASE_URL}/users/${id}/applications?limit=${LIMIT}&offset=${offset}`,
+              {
+                headers: {
+                  Authorization: jwt,
+                },
               },
-            }),
+            ),
           ]);
         const { name, phone, address, bio } = userInfo.item;
         setProfile((prev) => ({ ...prev, name, phone, address, bio }));
@@ -65,7 +77,13 @@ export default function MyProfile() {
             status,
           };
         });
-        setApplicationHistory(history);
+        setHistory((prev) => ({
+          ...prev,
+          type: userType as "employer" | "employee",
+          limit,
+          count,
+          items: history,
+        }));
       } catch (error) {
         if (axios.isAxiosError(error) && error.response) {
           const { message } = error.response.data;
@@ -74,21 +92,13 @@ export default function MyProfile() {
       }
     };
     fetchData();
-  }, [id]);
-  console.log(profile);
-
-  const tempResponse: ApplyHistoryProps = {
-    type: "employee",
-    limit: 5,
-    count: applicationHistory.length,
-    items: applicationHistory,
-  };
+  }, [id, page]);
 
   return (
     <Layout>
       <Profile />
       <Wrapper>
-        <ApplyHistory {...tempResponse} />
+        <ApplyHistory {...history} />
       </Wrapper>
     </Layout>
   );
