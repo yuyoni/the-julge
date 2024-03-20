d1a093bba0223ebdfcba64da25b3f45ad98efe5d
midcondria <hyukkind@naver.com>
2024-03-21 01:11:41 +0900

    ♻️ refactor: 디렉토리 정리

--- pages/my-profile/hooks/useApplicationHistory.ts
+++ pages/my-profile/hooks/useApplicationHistory.ts
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { ApplyHistoryProps } from "../ApplyHistory";
+import { ApplyHistoryProps } from "../components/ApplyHistory";
 import useCookie from "@/hooks/useCookies";
 import axios from "axios";
 
