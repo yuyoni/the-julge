990d53ea31fe39e5f362d50ff7b128b727442cb5
괴도소연\박소연 <rnlduq157@naver.com>
2024-03-22 08:43:42 +0900

    🔥 remove: 불필요한 코드 삭제

--- pages/my-shop/register/index.page.tsx
+++ pages/my-shop/register/index.page.tsx
@@ -1,18 +1,11 @@
 import Gnb from "@/components/Gnb";
-import { useUser } from "@/contexts/UserContext";
 import styled from "@emotion/styled";
-import DropDown from "@/components/DropDown";
 import MyShopForm from "../components/MyshopForm";
 
 export default function Register() {
-  const { userInfo } = useUser();
-
-  const shopId = userInfo?.item.shop?.item.id;
-  const userId = userInfo?.item.id;
-
   return (
     <>
-      <Gnb userType="employer" />
+      <Gnb />
       <StyledDiv>
         <h1>가게 정보</h1>
         <MyShopForm />
