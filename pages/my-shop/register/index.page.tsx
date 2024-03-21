4f7260c89e2094cacace265986814b7ead60ef4f
jobible <whtjdrud800@gmail.com>
2024-03-21 09:09:00 +0900

    ♻️ refactor: GNB userType 프롭스 제거

--- pages/my-shop/register/index.page.tsx
+++ pages/my-shop/register/index.page.tsx
@@ -4,7 +4,7 @@ import Input from "./components/Input";
 export default function Register() {
   return (
     <>
-      <Gnb userType="employer" />
+      <Gnb />
       <h1>가게 정보</h1>
       <Input label="가게 이름*" />
       <Input label="분류*" placeholder="선택" includeImage inputSize={600} />
