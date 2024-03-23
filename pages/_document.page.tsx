bc2e32c28ff644db54e101bb019e60ee6ed3f5be
YU YEON LEE <doli0116@naver.com>
2024-03-24 03:36:04 +0900

    ✨ feat: 메인페이지 favicon 및 타이틀 추가

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -4,11 +4,14 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
+        <link rel="icon" href="/images/favicon.ico" />
         <link
           href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css"
           rel="stylesheet"
           type="text/css"
         />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:type" content="website" />
       </Head>
       <body>
         <Main />
