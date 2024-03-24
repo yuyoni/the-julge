bbaec32584c6a024a5a805758ef70fdf53ef0a7a
jobible <whtjdrud800@gmail.com>
2024-03-24 22:10:52 +0900

    💄design: 전체공고 스켈레톤 ui 추가

--- pages/search/components/AllNotice/index.tsx
+++ pages/search/components/AllNotice/index.tsx
@@ -6,6 +6,8 @@ import PostList from "@/pages/search/components/PostList";
 import AllNoticeHeader from "@/pages/search/components/AllNotice/components/AllNoticeHeader";
 import useAllNotices from "../../hooks/useAllNotices";
 import { AllNoticeProps } from "../../types/type";
+import Post from "@/components/Post";
+import SkeletonUI from "@/components/Skeleton";
 
 export default function AllNotice({
   keyword,
@@ -34,7 +36,6 @@ export default function AllNotice({
         onApplyFilter={handleApplyFilter}
         keyword={keyword}
       />
-
       {isSuccess &&
         (noticeArray.length === 0 ? (
           <PostContent>
@@ -51,6 +52,14 @@ export default function AllNotice({
             </PaginationWrapper>
           </>
         ))}
+
+      {!isSuccess && (
+        <PostContent>
+          {Array.from({ length: 6 }).map((_, index) => (
+            <SkeletonUI key={index} height={367} />
+          ))}
+        </PostContent>
+      )}
     </AllNoticeList>
   );
 }
